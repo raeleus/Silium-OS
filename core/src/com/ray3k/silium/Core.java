9d7ef2c4b2c5a3d2a3f2d40ed5a70e787c58d14d
Raeleus <raymond.ray3k@gmail.com>
2019-06-09 11:58:46 -0700

    Added versioning.

--- core/src/com/ray3k/silium/Core.java
+++ core/src/com/ray3k/silium/Core.java
@@ -1,17 +1,11 @@
 package com.ray3k.silium;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Version;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class Core extends Game {
     public final static String VERSION = "1.0";
     @Override
     public void create () {
-        setScreen(new MenuScreen());
+        setScreen(new GameScreen());
     }
 }
--- core/src/com/ray3k/silium/GameScreen.java
+++ core/src/com/ray3k/silium/GameScreen.java
@@ -1,13 +1,16 @@
 package com.ray3k.silium;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
-public class MenuScreen implements Screen {
+public class GameScreen implements Screen {
     private Stage stage;
     private Skin skin;
     
@@ -15,6 +18,17 @@ public class MenuScreen implements Screen {
     public void show() {
         stage = new Stage(new ScreenViewport());
         skin = new Skin(Gdx.files.internal("ui/silium-ui.json"));
+    
+        Table root = new Table();
+        root.setFillParent(true);
+        stage.addActor(root);
+        
+        Table table = new Table();
+        table.setBackground(skin.getDrawable("menu-bar-tinted"));
+        root.add(table).growX().expandY().top();
+    
+        Label label = new Label("Silium OS ver. " + Core.VERSION,skin,"caption");
+        table.add(label).expandX().left().padLeft(6);
     }
     
     @Override
@@ -23,6 +37,11 @@ public class MenuScreen implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.act();
         stage.draw();
+        
+        if (Gdx.input.isKeyJustPressed(Input.Keys.F5)) {
+            dispose();
+            show();
+        }
     }
     
     @Override
