b29acaa87d232f151584a15ff0a5627f8e4a91aa
Raeleus <raymond.ray3k@gmail.com>
2019-06-09 12:44:04 -0700

    Revert "added typing label to gwt"
    This reverts commit 52ee3bfbe45ff6f577687a6101804b64a8a18f79.

--- core/src/com/ray3k/silium/GameScreen.java
+++ core/src/com/ray3k/silium/GameScreen.java
@@ -9,7 +9,6 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.rafaskoberg.gdx.typinglabel.TypingLabel;
 
 public class GameScreen implements Screen {
     private Stage stage;
@@ -28,7 +27,7 @@ public class GameScreen implements Screen {
         table.setBackground(skin.getDrawable("menu-bar-tinted"));
         root.add(table).growX().expandY().top();
     
-        TypingLabel label = new TypingLabel("Silium OS ver. " + Core.VERSION,skin,"caption");
+        Label label = new Label("Silium OS ver. " + Core.VERSION,skin,"caption");
         table.add(label).expandX().left().padLeft(6);
     }
     
