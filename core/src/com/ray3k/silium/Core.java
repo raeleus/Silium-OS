4ef2d0fb8dc0b84563d5c15f7733aabb8f98ff3a
Raeleus <raymond.ray3k@gmail.com>
2019-06-09 10:57:13 -0700

    Update tabs

--- core/src/com/ray3k/silium/Core.java
+++ core/src/com/ray3k/silium/Core.java
@@ -5,29 +5,28 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.ui.Container;
 
 public class Core extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
-
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    
+    @Override
+    public void create () {
+    
+    }
+    
+    @Override
+    public void render () {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+    
+    @Override
+    public void resize(int width, int height) {
+    
+    }
+    
+    @Override
+    public void dispose () {
+    
+    }
 }
--- desktop/src/com/ray3k/silium/desktop/DesktopLauncher.java
+++ desktop/src/com/ray3k/silium/desktop/DesktopLauncher.java
@@ -5,8 +5,8 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.ray3k.silium.Core;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Core(), config);
-	}
+    public static void main (String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new Core(), config);
+    }
 }
