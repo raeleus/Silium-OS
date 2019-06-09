7bc48748a449fc8818f2712f9b4ea674ae2cd680
Raeleus <raymond.ray3k@gmail.com>
2019-06-09 10:07:41 -0700

    Initial commit

---
+++ desktop/src/com/ray3k/silium/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ray3k.silium.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ray3k.silium.Core;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Core(), config);
+	}
+}
