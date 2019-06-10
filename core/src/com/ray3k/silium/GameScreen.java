cb1225f2bb356d3d2e40767554f50b510e65edf5
Raeleus <raymond.ray3k@gmail.com>
2019-06-09 21:48:30 -0700

    Added GameScreen UI

--- core/src/com/ray3k/silium/GameScreen.java
+++ core/src/com/ray3k/silium/GameScreen.java
@@ -5,22 +5,21 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.esotericsoftware.spine.SkeletonData;
 import com.esotericsoftware.spine.utils.TwoColorPolygonBatch;
 import com.rafaskoberg.gdx.typinglabel.TypingLabel;
 
 public class GameScreen implements Screen {
     private Stage stage;
     private Skin skin;
+    private final static String LONG_TEXT = "This is a very long text that I'm trying to use to develop my abilities in the naked arts. It is known throughout the land as an evil assassin supersoldier.";
     
     @Override
     public void show() {
         stage = new Stage(new ScreenViewport(), new TwoColorPolygonBatch());
+        Gdx.input.setInputProcessor(stage);
+        
         skin = Core.instance.assetManager.get("ui/silium-ui.json");
     
         Table root = new Table();
@@ -29,18 +28,97 @@ public class GameScreen implements Screen {
         
         Table table = new Table();
         table.setBackground(skin.getDrawable("menu-bar-tinted"));
-        root.add(table).growX().expandY().top();
+        root.add(table).growX();
     
-        TypingLabel label = new TypingLabel("Silium OS ver. " + Core.VERSION,skin,"caption");
-        table.add(label).expandX().left().padLeft(6);
+        TypingLabel typingLabel = new TypingLabel("Silium OS ver. " + Core.VERSION,skin,"caption");
+        table.add(typingLabel).expandX().left().padLeft(6);
+    
+        Button button = new Button(skin,"menu-settings");
+        table.add(button);
+    
+        button = new Button(skin,"menu-close");
+        table.add(button);
         
         root.row();
-        SpineDrawable.SpineDrawableTemplate template = new SpineDrawable.SpineDrawableTemplate();
-        SpineDrawable spineDrawable = new SpineDrawable(Core.instance.assetManager.get("ui/logo.json", SkeletonData.class),Core.instance.skeletonRenderer, template);
-//        spineDrawable.getAnimationState().setAnimation(0, "show", false);
-        spineDrawable.getAnimationState().addAnimation(0, "animation", false, 0);
-        Image image = new Image(spineDrawable);
-        root.add(image);
+        Table bottom = new Table();
+        root.add(bottom).grow();
+        
+        table = new Table();
+        bottom.add(table).growY().width(300);
+        
+        Table subTable = new Table();
+        table.add(subTable);
+        
+        Image image = new Image(skin,"icon-kreddits-tinted");
+        subTable.add(image);
+        
+        Label label = new Label("$300",skin);
+        subTable.add(label);
+    
+        image = new Image(skin,"icon-data-points-tinted");
+        subTable.add(image).spaceLeft(10);
+    
+        label = new Label("X5",skin);
+        subTable.add(label);
+    
+        image = new Image(skin,"icon-firewall-tinted");
+        subTable.add(image).spaceLeft(10);
+    
+        label = new Label("X5",skin);
+        subTable.add(label);
+    
+        image = new Image(skin,"icon-proxy-tinted");
+        subTable.add(image).spaceLeft(10);
+    
+        label = new Label("X5",skin);
+        subTable.add(label);
+        
+        table.row();
+        table.add().growY();
+        
+        table = new Table();
+        table.setBackground(skin.getDrawable("table-with-tab-tinted"));
+        bottom.add(table).grow();
+    
+        ButtonGroup<TextButton> buttonGroup = new ButtonGroup<TextButton>();
+        
+        TextButton textButton = new TextButton("TTY1", skin, "tab");
+        table.add(textButton);
+        buttonGroup.add(textButton);
+    
+        textButton = new TextButton("TTY2", skin, "tab");
+        table.add(textButton);
+        buttonGroup.add(textButton);
+        
+        table.add().expandX();
+        
+        table.row();
+        subTable = new Table();
+        table.add(subTable).grow().padTop(8).colspan(3);
+        
+        typingLabel = new TypingLabel(LONG_TEXT, skin);
+        typingLabel.setWrap(true);
+        subTable.add(typingLabel).growX().colspan(2);
+        
+        subTable.defaults().padTop(5);
+        subTable.row();
+        label = new Label("root/documents>", skin);
+        subTable.add(label).expandY().top();
+        
+        TextField textField = new TextField("", skin);
+        subTable.add(textField).growX().expandY().top().minWidth(50);
+        
+        table = new Table();
+        table.setBackground(skin.getDrawable("table-with-tab-tinted"));
+        bottom.add(table).fillX().growY().maxWidth(200);
+        
+        textButton = new TextButton("Notes", skin, "tab");
+        table.add(textButton).left();
+        buttonGroup.add(textButton);
+    
+        table.row();
+        TextArea textArea = new TextArea("",skin);
+        table.add(textArea).grow().padTop(8).minWidth(50);
     }
     
     @Override
